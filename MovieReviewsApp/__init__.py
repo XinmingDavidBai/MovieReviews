c869f1f3bc8b10cdc30ebff5f388f6a35cdf533c
Jacob  Lomholt Eriksen <jle@infoba.dk>
2025-05-27 15:11:28 +0200

    search function dynamic

--- MovieReviewsApp/__init__.py
+++ MovieReviewsApp/__init__.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from .db import cur, conn
 from .queries import search_movies
 
-
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/search')
+def search():
     query = request.args.get('query', '').strip()
     results = search_movies(query) if query else []
-    return render_template("index.html", query=query, results=results)
+    return jsonify(results)
+
